6a0b1545b4cb1c71cac86c0fd5d93c32b9099466
Sanjh Dixit <93241983+sanjhdixit@users.noreply.github.com>
2022-02-07 08:57:36 -0800

    Add files via upload

---
+++ playground (15).swift
@@ -0,0 +1,13 @@
+var names = ["harsh","Rashi","jiya","parth"]
+names.append("krati")
+    print(names)
+if names.contains("diya")
+{
+    print("TRUE")
+}
+else
+{
+    print("FALSE")
+}
+names.insert("harini" ,at:2)
+print(names)
